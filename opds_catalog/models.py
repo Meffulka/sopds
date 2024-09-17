7d14e58696692d9c7e78122223bf7d8c61a0a81c
Meffulka <zmeffulka@gmail.com>
2024-09-17 14:04:02 +0300

    fix datetime

--- opds_catalog/models.py
+++ opds_catalog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import datetime
+from datetime import datetime
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils.translation import ugettext_lazy as _lazy
 
